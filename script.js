c47e3aabbeb4dae8ce017d5d59b6a82511c864e6
Robert Teets <teetsrobert@gmail.com>
2022-07-10 12:09:21 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function loginOrLogout(element) {
+    if (element.innerText == 'Login!') {
+        element.innerText = 'Logout!';
+    }
+    else {
+        element.innerText = 'Login!';
+    }
+}
+
+function removeElement(element) {
+    element.remove();
+}
